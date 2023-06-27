2b49df9c9d8aa18ba9ba41630912e3ff74e1c35a
JessSoler <121449687+JessSoler@users.noreply.github.com>
2023-06-27 18:34:31 -0300

    Adicionado por Upload

---
+++ ATLETICA/HTML/config.php
@@ -0,0 +1,19 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '200601';
+    $dbName = 'formulario-atletica';
+    
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+     // if($conexao->connect_errno)
+     //{
+    // echo "Erro";
+    // }
+     // else
+     //{
+      // echo "Conexão efetuada com sucesso";
+    //}
+
+?>
